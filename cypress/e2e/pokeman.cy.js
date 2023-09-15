bdca3918adc9f4e23a8bed9cf98cb1358f7c8eb1
Awel Eshetu <aweloe@gmail.com>
2023-09-15 12:21:37 +0300

    Fix port
     - config port

--- cypress/e2e/pokeman.cy.js
+++ cypress/e2e/pokeman.cy.js
@@ -1,6 +1,6 @@
 describe('Pokedex', function() {
   beforeEach(function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://127.0.0.1:5000')
   })
 
   it('front page can be opened', function() {
